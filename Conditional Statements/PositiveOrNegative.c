b5a5080ab7139d14f021ef5fce8f54b0686be155
ticklemeelmo <ticklemeelmo132@gmail.com>
2020-08-20 19:28:48 +0000

    This program tells if a number is positive or negative

---
+++ Conditional Statements/PositiveOrNegative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int number = get_int("Imput a number: ");
+    
+    if (number < 0)
+    {
+        printf("%d is a negative number\n\n", number);
+    }
+    else
+    {
+        printf("%d is a positve number\n\n", number);
+    }
+}
